ae784e914e33a715fd3f2cf5025cb1bd0e6d5594
Engin <engin@turngs.com>
2018-02-26 18:36:31 +0300

    Init

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x42, 0xce, 0x59, 0x5a, 0x59, 0x44, 0x3e, 0x5f, 0x54, 0xE6, 0xa0, 0x17, 0x10, 0xa9, 0x5a, 0xf0 } };
+}
